05c414f9ddf5b82930a5b67ca88104410219464e
lanlandetiankong <695605813@qq.com>
2020-07-18 15:01:01 +0800

    200718 根据运行环境切换值

--- src/config/constantParams.js
+++ src/config/constantParams.js
@@ -1,33 +1,73 @@
-const constantParams ={
-    props:{
-        upload:{
-            location:{
-                prefix:dealGetValueByEnv("M:\\static_dir\\","C:\\swordsman\\projects\\egg_manager\\static_dir"),
-                img:"\\img"
-            },
-            url:{
-                prefix:dealGetValueByEnv("http:\\\\localhost:8090","http:\\\\47.99.160.215:8090")
-            },
-            project:{
-                name:"egg_manager"
-            }
-        }
-    },
-    defalutObj:{    //一些常用的默认值
-        avatarUrl:"/static/img/user/panda_egg.jpg"
+const constantParams = dealGetConstantParamsByEnv();
+
+function dealGetConstantParamsByEnv() {
+    if(process.env.NODE_ENV === 'development'){
+        //开发环境
+        return dealGetDevConstantParams();
+    }   else if(process.env.NODE_ENV === "production"){
+        //生产环境
+        return dealGetProdConstantParams();
+    }   else if(process.env.NODE_ENV === "testing"){
+        //测试环境
+        return dealGetTestConstantParams();;
     }
 }
 
 /**
- * 根据 当前环境取得对应的值
- * devVal:本地开发用的值
- * prodVal 生产环境使用的值
+ * 取得 开发环境 参数
  */
-function dealGetValueByEnv(devVal,prodVal) {
-    if(process.env.NODE_ENV === 'development'){
-        return devVal ;
-    }   else {
-        return prodVal ;
+function dealGetDevConstantParams() {
+    return {
+        props:{
+            upload:{
+                location:{
+                    prefix:"M:\\static_dir\\",
+                    img:"\\img"
+                },
+                url:{
+                    prefix:"http:\\\\localhost:8090"
+                },
+                project:{
+                    name:"egg_manager"
+                }
+            }
+        },
+        defalutObj:{    //一些常用的默认值
+            avatarUrl:"/static/img/user/panda_egg.jpg"
+        }
     }
 }
+/**
+ * 取得 生产环境 参数
+ */
+function dealGetProdConstantParams() {
+    return {
+        props:{
+            upload:{
+                location:{
+                    prefix:"C:\\swordsman\\projects\\egg_manager\\static_dir",
+                    img:"\\img"
+                },
+                url:{
+                    prefix:"http:\\\\47.99.160.215:8090"
+                },
+                project:{
+                    name:"egg_manager"
+                }
+            }
+        },
+        defalutObj:{    //一些常用的默认值
+            avatarUrl:"/static/img/user/panda_egg.jpg"
+        }
+    }
+}
+/**
+ * 取得 测试环境 参数
+ */
+function dealGetTestConstantParams() {
+    return {
+
+    }
+}
+
 export  default constantParams ;
